615ab098022614b503c02bcda3828d6edddfa928
yukiyuki <2726165926@qq.com>
2024-11-26 22:01:19 +0800

    2024.11.26.10:01pm

---
+++ 09_dydatastruct_1.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+int main(){
+    vector <int> nums;
+    int _;
+    while (1)
+    {
+        cin >> _;
+        if(_==0)
+            break;
+        else
+            nums.push_back(_);
+    }
+    
+    if(nums.empty())
+        printf("NULL");
+    else{
+        for(auto &a:nums){
+            cout << a <<" ";
+        }
+    }
+    
+}*///用vector秒了，真的是招笑了
+struct ListNode
+{
+    int val;
+    struct ListNode* next;
+};
+
+void insert(ListNode *cur,ListNode *toadd){
+    ListNode
+}
+
